9c5c9b2f9798944c1164fb50a18c0d3966f77381
Marcos Henrique <marcos2890@outlook.com>
2021-05-01 12:37:45 -0300

    Atualizando

---
+++ SmartStaf_README/pratica/03-juntar-arrays/juntar-arrays/script.js
@@ -0,0 +1,7 @@
+var num1 = [0, 1, 2, 3, 4, 5];
+var palavras = ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"];
+var num2 = [6, 7, 8, 9, 10];
+
+var arrays = num1.concat(palavras, num2);
+
+document.write(arrays);
