c882286a471233657ac158123a26d8348eec6436
Marcos Henrique <marcos2890@outlook.com>
2021-04-30 22:48:38 -0300

    Atualizando

---
+++ SmartStaf_README/pratica/01-smart-staff/smart-staff/script.js
@@ -0,0 +1,21 @@
+contador=0 
+
+for(i=-71;i<103;i++)
+{
+  if(i%3==0)
+  {
+  contador=contador+i;
+  document.write(i+ "\n Smart: ")
+  document.write("<br> <br>")
+}
+} 
+
+for(i=-71;i<103;i++)
+{
+  if(i%5==0)
+  {
+  contador=contador+i;
+  document.write(i+ "\n Staff: ")
+  document.write("<br> <br>")
+}
+} 
---
+++ SmartStaf_README/pratica/02-inversao-array/Inversao-array/script.js
@@ -0,0 +1,6 @@
+var palavras = new Array ("The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog");
+
+        palavras.reverse();
+        document.write(palavras);
+    
+    
